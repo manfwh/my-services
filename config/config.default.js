97014b5802f4a618593b7aad366610f848ba7dc7
manfwh <841533182@qq.com>
2018-06-09 23:10:34 +0800

    接入阿里性能平台

--- config/config.default.js
+++ config/config.default.js
@@ -8,6 +8,10 @@ module.exports = appInfo => {
 
   // add your config here
   config.middleware = [];
-
+  config.alinode = {
+    // 从 `Node.js 性能平台` 获取对应的接入参数
+    appid: '44727',
+    secret: 'd58419f64b2c5386a41b97a615c85506d596669a'
+  }
   return config;
 };
