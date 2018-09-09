9c23ed66bb065c92caf0269ffa068a2c9fe7e523
manfwh <841533182@qq.com>
2018-09-09 11:36:37 +0800

    fix

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -5,8 +5,8 @@ const Controller = require('egg').Controller;
 
 class AiController extends Controller {
   async index() {
+    const { ctx } = this;
     ctx.body = "hello wx!"
   }
 }
-
 module.exports = AiController;
