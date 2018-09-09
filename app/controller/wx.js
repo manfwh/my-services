e1008385f37d71ff10faec330533282bb70fef94
manfwh <841533182@qq.com>
2018-09-09 15:06:37 +0800

    m

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -7,7 +7,9 @@ const wechat = require('co-wechat');
 
 class weChatController extends Controller {
   async index() {
-   
+    const { ctx, service, config } = this;
+    console.log(ctx.request.body)
+    ctx.body = 2;
   }
 }
 // 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
@@ -16,7 +18,6 @@ weChatController.prototype.wechat = wechat({
   appid: 'wx21d8e7672a056d28',
   encodingAESKey: 'QaPCJCEkazygiar2LQzUDBYTcGEMvzRbFR8kERR1BvD'
 }).middleware(async (message, ctx) => {
-  console.log(message)
-  ctx.body = message
+  return 'Hello world!';
 });
 module.exports = weChatController;
