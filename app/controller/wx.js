b859f6e09e99e2e790fea2fa28a334756ea72cb0
manfwh <841533182@qq.com>
2018-09-09 11:49:46 +0800

    add check

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -1,12 +1,30 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
+const crypto = require('crypto')
+const token = 'shfwh'
 
 class AiController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = "hello wx!"
+    var signature = ctx.request.body.signature;
+    var timestamp = ctx.request.body.timestamp;
+    var nonce = ctx.request.body.nonce;
+    var echostr = ctx.request.body.echostr;
+    var array = new Array(token, timestamp, nonce);
+    array.sort();
+    var str = array.toString().replace(/,/g, "");
+
+    //2. 将三个参数字符串拼接成一个字符串进行sha1加密
+    var sha1Code = crypto.createHash("sha1");
+    var code = sha1Code.update(str, 'utf-8').digest("hex");
+    //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+    if (code === signature) {
+      ctx.body = echostr
+    } else {
+      ctx.body = "hello wx!"
+    }
+    
   }
 }
 module.exports = AiController;
