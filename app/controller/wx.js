c865f9a3ce80afd19dfcf735de2b8f3f00794afe
manfwh <841533182@qq.com>
2018-09-09 11:53:53 +0800

    m

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -18,6 +18,8 @@ class AiController extends Controller {
     //2. 将三个参数字符串拼接成一个字符串进行sha1加密
     var sha1Code = crypto.createHash("sha1");
     var code = sha1Code.update(str, 'utf-8').digest("hex");
+    console.log(ctx.request.body)
+    console.log(code)
     //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
     if (code === signature) {
       ctx.body = echostr
