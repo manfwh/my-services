54c805eb3f178710ac4833523afa30c2646fc834
manfwh <841533182@qq.com>
2018-09-09 12:01:08 +0800

    query

--- app/controller/wx.js
+++ app/controller/wx.js
@@ -7,10 +7,10 @@ const token = 'shfwh'
 class AiController extends Controller {
   async index() {
     const { ctx } = this;
-    var signature = ctx.request.body.signature;
-    var timestamp = ctx.request.body.timestamp;
-    var nonce = ctx.request.body.nonce;
-    var echostr = ctx.request.body.echostr;
+    var signature = ctx.query.signature;
+    var timestamp = ctx.query.timestamp;
+    var nonce = ctx.query.nonce;
+    var echostr = ctx.query.echostr;
     var array = new Array(token, timestamp, nonce);
     array.sort();
     var str = array.toString().replace(/,/g, "");
@@ -18,7 +18,7 @@ class AiController extends Controller {
     //2. 将三个参数字符串拼接成一个字符串进行sha1加密
     var sha1Code = crypto.createHash("sha1");
     var code = sha1Code.update(str, 'utf-8').digest("hex");
-    console.log(ctx.request.body)
+    console.log(ctx.query)
     console.log(code)
     //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
     if (code === signature) {
